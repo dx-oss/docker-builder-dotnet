192219b5292c32c828c51f2b52f20ebff80ef75c
Christian Sakshaug <christian@dx.no>
2021-07-02 12:31:08 +0200

    Updating and working with dotnet 5.0

--- build.sh
+++ build.sh
@@ -3,25 +3,29 @@
 pwd=$(pwd)
 
 shaver=$(git rev-parse --short HEAD)
+push=${PUSH:-1}
 
 ORG=dxdx
 NAME=docker-builder-dotnet
 
 IMAGE_TAG_PREFIX=$ORG/$NAME:
 
+IMAGE_NET50=mcr.microsoft.com/dotnet/sdk:5.0
 IMAGE_NETCORE31=mcr.microsoft.com/dotnet/core/sdk:3.1
 IMAGE_NETCORE30=mcr.microsoft.com/dotnet/core/sdk:3.0
 IMAGE_NETCORE22=mcr.microsoft.com/dotnet/core/sdk:2.2
 
+BRANCH_NET50=sdk-5.0
 BRANCH_NETCORE31=sdk-3.1
 BRANCH_NETCORE30=sdk-3.0
 BRANCH_NETCORE22=sdk-2.2
 
+TAGS_NET50="v5 v5.0 v5.0.0"
 TAGS_NETCORE31="v3 v3.1 v3.1.2"
 TAGS_NETCORE30="v3.0 v3.0.0"
 TAGS_NETCORE22="v2 v2.2 v2.2.0"
 
-VERSIONS="NETCORE22 NETCORE30 NETCORE31"
+VERSIONS="NETCORE22 NETCORE30 NETCORE31 NET50"
 VERSIONS=${1:-$VERSIONS}
 
 function log {
@@ -50,12 +54,15 @@ function build {
 
     run cp common/* $branch    
     run cd $branch
+    run docker pull $image
     run docker build --build-arg '"DOCKER_IMAGE=$image"' -t $tag .
     run docker tag $tag $tag_major
     #run docker tag $tag $tag_shaver
-    run docker push $tag
-    run docker push $tag_major
-    #run docker push $tag_shaver
+    if [ $push -gt 0 ]; then
+        run docker push $tag
+        run docker push $tag_major
+        #run docker push $tag_shaver
+    fi
     for c in `ls ../common`
     do
         run rm $c
